a550fd18b5ce15d33005b0ad0801c7ec9e534edb
islemdouakha <islemdouakha11@gmail.com>
2024-01-08 20:10:25 +0100

    modified:   projet_v1/Classes.py 	new file:   projet_v1/TP.py

--- projet_v1/Classes.py
+++ projet_v1/Classes.py
@@ -1,10 +1,9 @@
 
 
 
-
 class Document:
 
-    def __init__(self, titre="", auteur="", date="", url="", texte=""):
+    def __init__(self, titre="", auteur="", date="", url="", texte="",type=""):
         self.titre = titre
         self.auteur = auteur
         self.date = date
@@ -19,6 +18,8 @@ class Document:
 
     def __str__(self):
         return f"{self.titre}, par {self.auteur}"
+    def getType(self):
+        pass
 
 
 
@@ -34,3 +35,36 @@ class Author:
         self.production.append(production)
     def __str__(self):
         return f"Auteur : {self.name}\t# productions : {self.ndoc}"
+
+class RedditDocument(Document):
+    def __init__(self, titre="", auteur="", date="", url="", texte="", nbcmnts="",type=""):
+        super().__init__( titre, auteur, date, url, texte,type)
+        self.nbcmnts = nbcmnts
+
+    def get_nbcmnts(self):
+        return self.nbcmnts
+    
+    def set_nbcmnts(self, nb):
+        self.nbcmnts = nb
+        
+    def __str__(self):
+        return f"Titre : {self.titre}\tAuteur : {self.auteur}\tDate : {self.date}\tURL : {self.url}\tTexte : {self.texte}\tNombre de commentaires : {self.nbcmnts}\tType : {self.getType()}"
+    def getType(self):
+        return "RedditDocument"
+
+class ArxivDocument(Document):
+
+    def __init__(self, titre="", date="", auteur="", url="", texte="",type=""):
+        super().__init__(titre,auteur,date, url, texte,type)
+        self.auteur = auteur.split(',')
+        
+    def get_auteurs(self):
+        return self.auteur
+    
+    def set_auteurs(self, auteurs):
+        self.auteur = auteurs.split(",")
+    
+    def getType(self):
+        return "ArxivDocument"
+    def __str__(self):
+        return f"Titre : {self.titre}\tAuteur : {self.auteur}\tDate : {self.date}\tURL : {self.url}\tTexte : {self.texte}\tType : {self.getType()}"
