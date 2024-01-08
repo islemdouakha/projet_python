c0864fe9bcf76db80fd9ce2b7e02e9c6b293f489
islemdouakha <islemdouakha11@gmail.com>
2024-01-08 19:49:52 +0100

    new file:   projet_v1/Classes.py

---
+++ projet_v1/Classes.py
@@ -0,0 +1,36 @@
+
+
+
+
+class Document:
+
+    def __init__(self, titre="", auteur="", date="", url="", texte=""):
+        self.titre = titre
+        self.auteur = auteur
+        self.date = date
+        self.url = url
+        self.texte = texte
+
+
+
+    def __repr__(self):
+        return f"Titre : {self.titre}\tAuteur : {self.auteur}\tDate : {self.date}\tURL : {self.url}\tTexte : {self.texte}\t"
+
+
+    def __str__(self):
+        return f"{self.titre}, par {self.auteur}"
+
+
+
+
+class Author:
+    def __init__(self, name):
+        self.name = name
+        self.ndoc = 0
+        self.production = []
+
+    def add(self, production):
+        self.ndoc += 1
+        self.production.append(production)
+    def __str__(self):
+        return f"Auteur : {self.name}\t# productions : {self.ndoc}"
